5ded0b1780d0b815e57baa7fd5932c63f010ae39
zxinying <56820736+zxinying@users.noreply.github.com>
2020-01-09 16:53:47 +0800

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,3 +1,12 @@
+/*百度统计*/
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?bbbbf5e970e28a288aa7e34c1996a95a";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+/**/
 $(function() {
   'use strict';
 
